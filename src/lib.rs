3b0dd1da0abf3b311d8045939ae2e14041e3dd07
wyenox <227889946+wyenox@users.noreply.github.com>
2025-12-15 17:48:21 -0300

    bring the code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod clients;
+pub mod controllers;
+pub mod mcp;
+pub mod protocol;
+pub mod utils;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod prelude;
