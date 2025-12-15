440133e098305c6ec4dee64d7c2a3a06b9235b2a
wyenox <227889946+wyenox@users.noreply.github.com>
2025-12-15 18:30:29 -0300

    explain in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! Crate reserved for https://github.com/moly-ai/aitk, under active development.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+compile_error!("Use from https://github.com/moly-ai/aitk instead.");
